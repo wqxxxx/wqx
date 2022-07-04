b67e032165607b3d48abdfb07eeaf36cb7bdb831
wqx <464731443@qq.com>
2022-07-04 16:34:51 +0800

    lodash

--- lodash/wqxxxx-lodash.js
+++ lodash/wqxxxx-lodash.js
@@ -1,4 +1,5 @@
 var wqxxxx = {
+    // 创建一个数组，将它们按size的长度分
     chunk: function (array, size = 1) {
         var a = []
         var len = array.length
@@ -11,6 +12,7 @@ var wqxxxx = {
     },
 
 
+    // 创建一个删除所有错误值的数组：
     compact: function (array) {
         var a = []
         for (var i = 0; i < array.length; i++) {
@@ -22,6 +24,7 @@ var wqxxxx = {
     },
 
 
+    // 数组从删除的第n个元素开始创建一个切片
     drop: function (array, n = 1) {
         var len = array.length
         var a = []
@@ -36,6 +39,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个数组从末尾删除n个元素的切片
     dropRight: function (array, n = 1) {
         var len = array.length
         for (var i = 0; i < n; i++) {
@@ -45,6 +49,7 @@ var wqxxxx = {
     },
 
 
+    // 数组用value的值填充，
     fill: function (array, value, start = 0, end = array.length) {
         for (var i = start; i < end; i++) {
             array[i] = value
@@ -54,6 +59,7 @@ var wqxxxx = {
 
 
 
+    // 展平数组一次
     flatten: function (array) {
         var a = []
         for (var i = 0; i < array.length; i++) {
@@ -71,6 +77,7 @@ var wqxxxx = {
 
 
 
+    // 递归展平数组
     flattenDeep: function (array) {
         var a = []
         for (var i = 0; i < array.length; i++) {
@@ -89,6 +96,7 @@ var wqxxxx = {
 
 
 
+    // 返回一个由key - value组成的对象
     fromPairs: function (pairs) {
         var a = {}
         for (var i = 0; i < pairs.length; i++) {
@@ -99,6 +107,7 @@ var wqxxxx = {
 
 
 
+    // 返回数组的第一个元素
     head: function (array) {
         var len = array.length
         if (len == 0) {
@@ -109,6 +118,7 @@ var wqxxxx = {
     },
 
 
+    // 搜索value的值在第几次出现在哪个位置
     indexOf: function (array, value, fromIndex = 0) {
         var len = array.length
         for (var i = fromIndex; i < len; i++) {
@@ -120,6 +130,7 @@ var wqxxxx = {
 
 
 
+    // 获取除最后一个元素之外的所有数组
     initial: function (array) {
         array.pop()
         return array
@@ -127,6 +138,7 @@ var wqxxxx = {
 
 
 
+    // 将所有元素用separator的值连接
     join: (array, separator = ',') => {
         var a = array[0] + ''
         for (var i = 1; i < array.length; i++) {
@@ -137,12 +149,14 @@ var wqxxxx = {
 
 
 
+    // 获取最后一个元素
     last: function (array) {
         return array[array.length - 1]
     },
 
 
 
+    // 将数组中包含value的值都删除
     pull: function (array, ...values) {
         var s = [...values]
         var a = array
@@ -159,6 +173,7 @@ var wqxxxx = {
 
 
 
+    // 反向数组
     reverse: function (array) {
         var a = []
         for (var i = array.length - 1; i >= 0; i--) {
@@ -169,6 +184,7 @@ var wqxxxx = {
 
 
 
+    // 获取除第一个元素之外的所有数组
     tail: function (array) {
         array.shift()
         return array
@@ -176,6 +192,7 @@ var wqxxxx = {
 
 
 
+    // 从n个元素的数组
     take: function (array, n = 1) {
         var a = []
         if (n >= array.length) {
@@ -189,6 +206,7 @@ var wqxxxx = {
 
 
 
+    // 从末尾开始获取第n个元素
     takeRight: function (array, n = 1) {
         var a = []
         var len = array.length
@@ -203,6 +221,7 @@ var wqxxxx = {
 
 
 
+    // 合并数组，并且元素不重复
     union: function (...array) {
         var arr = []
         var a = arr.concat(...array)         //   拼接数组
@@ -217,6 +236,7 @@ var wqxxxx = {
 
 
 
+    // 创建无重复数组
     uniq: function (array) {
         var a = []
         for (var i = 0; i < array.length; i++) {
@@ -229,6 +249,7 @@ var wqxxxx = {
 
 
 
+    // 返回array中没有value的值的数组
     without: function (array, ...values) {
         var arr = []
         for (var i = 0; i < array.length; i++) {
@@ -241,6 +262,7 @@ var wqxxxx = {
 
 
 
+    // 返回类似数组的值的长度或对象自己的可枚举字符串键控属性的数量
     size: function (collection) {
         var a = Object.keys(collection)
         var len = a.length
@@ -249,6 +271,7 @@ var wqxxxx = {
 
 
 
+    // 检查数组是否包含src的值，返回真假
     isMatch: function (obj, src) {
         for (var key in src) {
             if (src[key] && typeof src[key] === 'object') {
@@ -266,6 +289,7 @@ var wqxxxx = {
 
 
 
+    // 给定对象和部分执行比较，如果有等效值返回真，否则返回假
     matches: function (source) {
         return function (obj) {
             for (var key in source) {
@@ -279,6 +303,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个调用的函数，函数最多带n个参数
     ary: function (func, n = func.length) {
         return function (...args) {
             return func.call(this, ...args.slice(0, n))
@@ -287,6 +312,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个最多接受一个参数的函数
     unary: function (func) {
         return function (...args) {
             return func(...args.slice(0, 1))
@@ -296,6 +322,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个否定 predicate 结果的函数
     negate: function (predicate) {
         return function (...args) {
             return !predicate(...args)
@@ -304,6 +331,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个函数，该函数使用函数的this绑定和一个参数数组进行调用
     spread: function (func) {
         return function (ary) {
             return func.apply(this, ary)
@@ -312,6 +340,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个接受n个参数的函数，如果至少提供了多个参数，则f调用返回其结果，或者返回一个接受剩余参数的函数
     curry: function (f, n = f.length) {
         return function (...args) {
             if (args.length < n) {
@@ -324,6 +353,7 @@ var wqxxxx = {
 
 
 
+    // 创建一个使用反转参数调用的函数
     flip: function (func) {
         return function (...args) {
             return func(...args.reverse())
