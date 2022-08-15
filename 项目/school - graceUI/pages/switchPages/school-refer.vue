9efc5d572f743dca637f59e3a3c29a313770f192
wqx <464731443@qq.com>
2022-08-15 22:32:14 +0800

    项目

--- 项目/school - graceUI/pages/switchPages/school-refer.vue
+++ 项目/school - graceUI/pages/switchPages/school-refer.vue
@@ -40,13 +40,13 @@
 						</view>
 
 						<!-- 作业上传 -->
-						<view class="gui-form-item gui-border-b">
+						<view class="gui-form-item gui-border-b ">
 							<text class="gui-form-label gui-secondary-text">作业上传</text>
-							<view class="gui-margin-top gui-bg-gray gui-text-center" style="padding:25rpx;" @tap="chooseImg">
+							<view class="gui-margin-top gui-bg-gray gui-text-center refer" @tap="chooseImg">
 								<text class="gui-text-small gui-color-blue">点击这里选择文件</text>
+								<text class="totalname">{{ total }}</text>
 							</view>
 						</view>
-						<view>{{ total }}</view>
 					</view>
 					<!-- 作业描述 -->
 					<view class="gui-margin-top gui-padding-x"><text class="gui-h6 gui-secondary-text" style="height: 28rpx;">作业描述</text></view>
@@ -190,6 +190,14 @@ export default {
 			fromData.workDescribe = this.formData.workDescribe;
 			console.log(fromData);
 
+			if (!fromData.workImage) {
+				uni.showToast({ title: '请上传作业', icon: 'none' });
+			} else {
+				this.sub();
+			}
+		},
+
+		sub: function() {
 			uni.request({
 				url: `${getApp().globalData.baseUrl}/yuecheng/work/add`,
 				method: 'POST',
@@ -225,6 +233,7 @@ export default {
 				}
 			});
 		},
+
 		//文件上传
 		chooseImg: function() {
 			uni.chooseImage({
@@ -241,7 +250,7 @@ export default {
 						// },
 						success: uploadFileRes => {
 							let imgUrl = JSON.parse(uploadFileRes.data).data[0].url;
-							console.log(imgUrl);
+							//console.log(imgUrl);
 							let a = imgUrl.split('\/');
 							let b = a[a.length - 1];
 							this.total = b;
@@ -271,5 +280,14 @@ export default {
 
 .sub {
 	width: 200rpx;
+}
+
+.refer {
+	margin-top: 0;
+	padding: 25rpx;
+}
+
+.totalname {
+	font-size: 10rpx;
 }
 </style>
